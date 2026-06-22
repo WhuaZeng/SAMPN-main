39ad0df10494e57f0f43fd8fabcca81afacaf6f4
ZWH <143571497+WhuaZeng@users.noreply.github.com>
2026-06-22 21:46:12 +0800

    Add files via upload

---
+++ configs/__init__.py
@@ -0,0 +1,13 @@
+# 配置模块初始化文件
+from .config import *
+
+__all__ = [
+    'DATA_CONFIG',
+    'FEW_SHOT_CONFIG', 
+    'MODEL_CONFIG',
+    'TRAINING_CONFIG',
+    'MAML_CONFIG',
+    'PROTONET_CONFIG',
+    'VISUALIZATION_CONFIG',
+    'EXPERIMENT_CONFIG'
+]
